077f09d97e7797fb920d56c8b991b30f1f4fcbab
YunSung12 <107446074+YunSung12@users.noreply.github.com>
2023-03-11 14:20:42 +0900

    Update inst_to_reg_module.v

--- soykim/single/inst_to_reg_module.v
+++ soykim/single/inst_to_reg_module.v
@@ -1,9 +1,9 @@
 
 module Inst_to_reg_module (instruction, opcode, funct, rs, rt, rd, shamt, offset);
-	input [31:0] instruction;
+	input [31:0] instruction;	// get 32bit instruction
 	output [5:0] opcode, funct;
 	output [4:0] rs, rt, rd, shamt;
-	output [15:0] offset;
+	output [15:0] offset;		
 
 	assign opcode = instruction[31:26]; 
 	assign rs = instruction[25:21];  
@@ -11,5 +11,5 @@ module Inst_to_reg_module (instruction, opcode, funct, rs, rt, rd, shamt, offset
 	assign rd = instruction[15:11];  
 	assign shamt = instruction[10:6];
 	assign funct = instruction[5:0];
-	assign offset = instruction[15:0];   
+	assign offset = instruction[15:0];   // seperate 32bit instruction to [opcode, funct, rs, rt, rd, shamt, offset]
 endmodule
