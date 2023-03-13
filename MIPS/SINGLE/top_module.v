0f12d7050128febb99df900f39eb551b18e0a75d
ajinwon1212 <76693616+ajinwon1212@users.noreply.github.com>
2023-03-13 17:30:30 +0900

    Update top_module.v

--- MIPS/SINGLE/top_module.v
+++ MIPS/SINGLE/top_module.v
@@ -18,54 +18,46 @@
 
 module Top_single ( 
 	RESET, 
-	CLK, 
-	PC,			//[2.PC]
-	PC_next,		//[2.PC], [3.Add] (PC+4)
-	Read_address,		//[4.Instruction_memory]
-	Instruction,		//[4.Instruction_memory], [5.Control], [6.MUX], [7.Registers], [8.Sign_extend]
-	RegDst,			//[5.Control], [6.MUX]
-	Branch,			//[5.Control], [13.And]
-	MemRead,		//[5.Control], [12.Data_memory]
-	MemtoReg,		//[5.Control], 
-	ALUOp,			//[5.Control], [10.ALU_control]
-	MemWrite, 		//[5.Control], [12.Data_memory]
-	ALUSrc, 		//[5.Control], [6.MUX]
-	RegWrite, 		//[5.Control], [7.Registers]
-	MUX_a,			//[6.MUX]
-	MUX_b,			//[6.MUX]
-	MUX_sig,		//[6.MUX]
-	MUX_out,		//[6.MUX]
-	Read_register_1,	//[7.Registers]
-	Read_register_2, 	//[7.Registers]
-	Write_register,		//[6.MUX], [7.Registers]
-	Write_Data, 		//[6.MUX], [7.Registers]
-	Read_data_1,   		//[7.Registers], [9.ALU]
-	Read_data_2, 		//[7.Registers], [6.MUX]
-	Sign_extend,		//[8.Sign_extend], [11.Shift_left_2]
-	ADD_a, 			//[3.Add]
-	ADD_b, 			//[3.Add]
-	ADD_out, 		//[3.Add]
-	ALU_control, 		//[10.ALU_control], [9.ALU]
-	ALU_result,		//[9.ALU], [12.Data_memory] 
-	ALU_zero, 		//[9.ALU], [13.And]
-	AND_a,			//[13.And]
-	AND_b,			//[13.And]
-	AND_out,		//[13.And]
-	Address,		//[12.Data_memory] 
-	Write_data,		//[12.Data_memory] 
-	Read_data		//[12.Data_memory] 
+	CLK
 );
 
-	input clk, rst;
-	output wire [7:0] PC_next; 
-	output wire [5:0] opcode, funct;
-	output wire [4:0] rs, rt, rd, shamt, shift;
-	output wire RegDst, ALUSrc, MemtoReg, Regwrite, MemRead, MemWrite;// Branch, zero;
-	output wire [1:0] ALUOp;
-	output wire [31:0] instruction, readata1, readata2, ALUinput1, ALUinput2, ALUresult, Readata, outdata, sign_extend;
-	output wire [15:0] offset;
-	output wire [3:0] ALUcontrol;
-   
+   	input RESET;
+	input CLK;
+	
+	wire [31:0] PC;				//[2.PC]
+	wire [31:0] PC_next;			//[2.PC], [3.Add] (PC+4)
+	wire [31:0] Read_address;		//[4.Instruction_memory]
+	wire [31:0] Instruction;		//[4.Instruction_memory], [5.Control], [6.MUX], [7.Registers], [8.Sign_extend]
+	wire [1:0] RegDst;			//[5.Control], [6.MUX]
+	wire Branch; 				//[5.Control], [12.Data_memory]
+	wire [1:0] MemtoReg;			//[5.Control], 
+	wire [1:0] ALUOp;			//[5.Control], [10.ALU_control]
+	wire MemWrite; 				//[5.Control], [12.Data_memory]
+	wire [1:0] ALUSrc; 			//[5.Control], [6.MUX]
+	wire RegWrite; 				//[5.Control], [7.Registers]
+	wire [31:0] MUX_a;			//[6.MUX]
+	wire [31:0] MUX_b;			//[6.MUX]
+	wire [31:0] MUX_sig;			//[6.MUX]
+	wire [31:0] MUX_out;			//[6.MUX]
+	wire [4:0] Read_register_1;		//[7.Registers]
+	wire [4:0] Read_register_2; 		//[7.Registers]
+	wire [4:0] Write_register;		//[6.MUX], [7.Registers]
+	wire [31:0] Write_Data; 		//[12.Data_memory], [6.MUX], [7.Registers]
+	wire [31:0] Read_data_1;   		//[7.Registers], [9.ALU]
+	wire [31:0] Read_data_2; 		//[7.Registers], [6.MUX]
+	wire [31:0] Sign_extend;		//[8.Sign_extend], [11.Shift_left_2]
+	wire ADD_a; 				//[3.Add]
+	wire ADD_b; 				//[3.Add]
+	wire ADD_out; 				//[3.Add]
+	wire [3:0] ALU_control; 		//[10.ALU_control], [9.ALU]
+	wire [31:0] ALU_result;			//[9.ALU], [12.Data_memory] 
+	wire ALU_zero; 				//[9.ALU], [13.And]
+	wire [31:0] AND_a;			//[13.And]
+	wire [31:0] AND_b;			//[13.And]
+	wire [31:0] AND_out;			//[13.And]
+	output wire [31:0] Read_data;		//[12.Data_memory] 
+	
+	
 	Instruction_module Instruction_module(
 		//.rst(rst),
 		.PC_next(PC_next),
