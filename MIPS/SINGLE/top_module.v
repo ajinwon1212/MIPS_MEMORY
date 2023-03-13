c3e1eb489fe68f1af457ac3f0a0903bb6ade8ac0
ajinwon1212 <76693616+ajinwon1212@users.noreply.github.com>
2023-03-13 17:43:17 +0900

    Update top_module.v

--- MIPS/SINGLE/top_module.v
+++ MIPS/SINGLE/top_module.v
@@ -24,6 +24,10 @@ module Top_single (
    	input RESET;
 	input CLK;
 	
+	/*
+	output ;
+	*/
+	
 	wire [31:0] PC;				//[2.PC]
 	wire [31:0] PC_next;			//[2.PC], [3.Add] (PC+4)
 	wire [31:0] Read_address;		//[4.Instruction_memory]
@@ -46,109 +50,52 @@ module Top_single (
 	wire [31:0] Read_data_1;   		//[7.Registers], [9.ALU]
 	wire [31:0] Read_data_2; 		//[7.Registers], [6.MUX]
 	wire [31:0] Sign_extend;		//[8.Sign_extend], [11.Shift_left_2]
-	wire ADD_a; 				//[3.Add]
-	wire ADD_b; 				//[3.Add]
-	wire ADD_out; 				//[3.Add]
+	wire [31:0] ADD_a; 			//[3.Add]
+	wire [31:0] ADD_b; 			//[3.Add]
+	wire [31:0] ADD_out; 			//[3.Add]
 	wire [3:0] ALU_control; 		//[10.ALU_control], [9.ALU]
 	wire [31:0] ALU_result;			//[9.ALU], [12.Data_memory] 
 	wire ALU_zero; 				//[9.ALU], [13.And]
-	wire [31:0] AND_a;			//[13.And]
-	wire [31:0] AND_b;			//[13.And]
-	wire [31:0] AND_out;			//[13.And]
-	output wire [31:0] Read_data;		//[12.Data_memory] 
+	wire AND_a;				//[13.And]
+	wire AND_b;				//[13.And]
+	wire AND_out;				//[13.And]
+	wire [31:0] Read_data;		//[12.Data_memory] 
+	
+	// 2. PC
+	PC PC(
+		.CLK(CLK),		//IN
+		.RESET(RESET),		//IN
+		.PC_next(PC_next),	//IN
+		.PC(PC) 		//OUT
+	);
+	
+	// 3. Add
+	Add Add(
+		.ADD_a(ADD_a), 		//IN
+		.ADD_b(ADD_b),		//IN
+		.ADD_out(ADD_out)	//OUT
+	);
+	
+	// 4. Instruction_memory
+	Instruction_memory Instruction_memory(
+		.CLK(CLK)			//IN
+		.RESET(RESET),			//IN
+		.Read_address(Read_address),	//IN
+		.Instruction(Instruction)	//OUT
+	);
+	
+	// 5. Control
+	Control Control(
+		.in(instruction[31:26]), 	//IN
+		.RegDst(RegDst),		//OUT
+		.Branch(Branch),		//OUT
+		.MemRead(MemRead),		//OUT
+		.MemtoReg(MemtoReg),		//OUT
+		.ALUOp(ALUOp),			//OUT
+		.MemWrite(MemWrite),		//OUT
+		.ALUSrc(ALUSrc),		//OUT
+		.RegWrite(RegWrite)		//OUT
+	);
 	
 	
-	Instruction_module Instruction_module(
-		//.rst(rst),
-		.PC_next(PC_next),
-		.instruction(instruction)); 
-	
-	Inst_to_reg_module Inst_to_reg_module(
-		.instruction(instruction),
-		.opcode(opcode),
-		.funct(funct),
-		.rs(rs), 
-		.rt(rt), 
-		.rd(rd), 
-		.shamt(shamt),
-		.offset(offset)); 
-
-	Controlunit_module Controlunit_module(
-		.opcode(opcode),
-		.RegDst(RegDst),
-		.ALUSrc(ALUSrc),
-		.MemtoReg(MemtoReg),
-		.Regwrite(Regwrite),
-		.MemRead(MemRead),
-		.MemWrite(MemWrite),
-		//.Branch(Branch),
-		.ALUOp(ALUOp)
-		//.jump(jump)
-	);
-
-	Register_module Register_module(
-		.clk(clk),
-		.rst(rst),
-		.rs(rs), 
-		.rt(rt), 
-		.rd(rd), 
-		.shamt(shamt),
-		//.funct(funct),
-		.offset(offset),
-		.outdata(outdata),
-		.Regwrite(Regwrite), 
-		.RegDst(RegDst),
-		.readata1(readata1), 
-		.readata2(readata2), 
-		.sign_extend(sign_extend),
-		.shift(shift)
-		//.funct2(funct2)
-	);
-
-	reg_to_alu_module reg_to_alu_module(
-		.readata1(readata1), 
-		.readata2(readata2), 
-		.sign_extend(sign_extend),
-		.ALUSrc(ALUSrc),
-		.ALUOp(ALUOp),
-		.funct(funct),
-		.ALUinput1(ALUinput1), 
-		.ALUinput2(ALUinput2),
-		.ALUcontrol(ALUcontrol));
-
-	ALU_module ALU_module(
-		.ALUinput1(ALUinput1), 
-		.ALUinput2(ALUinput2),
-		//.ALUOp(ALUOp),
-		.ALUcontrol(ALUcontrol),
-		.shift(shift),
-        .ALUresult(ALUresult)
-		//.zero(zero)
-	);
-
-	Datamem_module Datamem_module(
-		.clk(clk),
-		.rst(rst),
-		.ALUresult(ALUresult), 
-		.readata2(readata2),
-		.MemWrite(MemWrite), 
-		.MemRead(MemRead),
-		.Readata(Readata));
-
-	Writeback_module Writeback_module(
-		.Readata(Readata), 
-		.ALUresult(ALUresult),
-		.MemToReg(MemtoReg),
-		.outdata(outdata));
-
-	PC_module PC_module(
-		.rst(rst),
-		.clk(clk), 
-		//.Branch(Branch), 
-		//.zero(zero),
-		.sign_extend(sign_extend),
-		.PC_next(PC_next),
-		.MemRead(MemRead)
-	);
- 
 endmodule
