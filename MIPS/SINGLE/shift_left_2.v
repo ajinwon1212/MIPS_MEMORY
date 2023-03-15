255933dee9bfdf71f447b453ccd343a4815e6d2f
YunSung12 <107446074+YunSung12@users.noreply.github.com>
2023-03-15 15:23:02 +0900

    Update shift_left_2.v

--- MIPS/SINGLE/shift_left_2.v
+++ MIPS/SINGLE/shift_left_2.v
@@ -1,8 +1,8 @@
 module Shift_left_2 (
-	input [31:0] Sign_extend,
+	input [31:0] Shift_left_2_IN,
 	output [31:0] Shift_left_2_OUT
 	);
 
-	assign Shift_left_2_OUT = Sign_extend << 2;
+	assign Shift_left_2_OUT = Shift_left_2_IN << 2;
 
 endmodule
