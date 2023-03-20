86aeaa4d8d90508dd2bbdcff67f2fb9e203598ec
YunSung12 <107446074+YunSung12@users.noreply.github.com>
2023-03-20 23:35:49 +0900

    Update mux.v

--- MIPS/SINGLE/mux.v
+++ MIPS/SINGLE/mux.v
@@ -9,3 +9,15 @@ module MUX (
 
 endmodule
 
+module FourOneMUX (
+	input [31:0] MUX_a, 
+	input [31:0] MUX_b,
+	input [31:0] MUX_c,
+	input [31:0] MUX_d,
+	input [1:0] MUX_sig,	
+	output [31:0] MUX_out
+	);
+
+	assign MUX_out = (MUX_sig[1] == 1'b0) ? ((MUX_sig[0] == 1'b0) ? MUX_a : MUX_b) : ((MUX_sig[0] == 1'b0) ? MUX_c : MUX_d);
+
+endmodule
