563c555c5605e8dc7e9c95628be0377d738bfbd2
ajinwon1212 <76693616+ajinwon1212@users.noreply.github.com>
2023-03-21 22:44:50 +0900

    Update ALU_control.v

--- MIPS/SINGLE/ALU_control.v
+++ MIPS/SINGLE/ALU_control.v
@@ -14,19 +14,13 @@ module ALU_control(
 );
 
 	input [5:0] ALU_control_IN; // 6bit input
-	input [1:0] ALUOp; // 2bit input
+	input [2:0] ALUOp; // 3bit input
 		
 	output [3:0] ALU_control; // 4 bit output
 
     always@(ALUOp or ALU_control_IN)begin // I referred to page 261 of the textbook.
         casex({ALUOp,ALU_control_IN})
-            8'b00_xxxxxx:AluCtrl=4'b0010; //lw / sw
-            8'b01_xxxxxx:AluCtrl=4'b0110; //beq(Branch equal) // I referred to page 260 of the textbook. (Fig. 4.12)
-            8'b1x_xx0000:AluCtrl=4'b0010; //add
-            8'b1x_xx0010:AluCtrl=4'b0110; //sub
-            8'b1x_xx0100:AluCtrl=4'b0000; //and
-            8'b1x_xx0101:AluCtrl=4'b0001; //or
-            8'b1x_xx1010:AluCtrl=4'b0111; //slt
+            9'b111_xxxxxx:AluCtrl=4'b1111; //Do nothing in ALU
         endcase
     end
     
