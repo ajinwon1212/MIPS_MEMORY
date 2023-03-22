0fdd669e17c95e4ead4ec46235b3be3a016a281c
ajinwon1212 <76693616+ajinwon1212@users.noreply.github.com>
2023-03-22 13:25:38 +0900

    Update Data_memory.v

--- MIPS/SINGLE/Data_memory.v
+++ MIPS/SINGLE/Data_memory.v
@@ -1,19 +1,18 @@
-module Data_Memory(Address, WriteData, MemWrite, MemRead, Read_data); 
+module Data_Memory(Address, Write_Data, MemWrite, MemRead, Read_data); 
+	
+	input [31:0] Address; 	// Input Address 
+	input [31:0] Write_Data; // Data that needs to be written into the address 
+	input MemWrite; 		// Control signal for memory write 
+	input MemRead; 			// Control signal for memory read 
 
-    input [31:0] Address; 	// Input Address 
-    input [31:0] WriteData; // Data that needs to be written into the address 
-    input MemWrite; 		// Control signal for memory write 
-    input MemRead; 			// Control signal for memory read 
-
-    output reg [31:0] Read_data; // Contents of memory location at Address
-
-    reg 	[31:0] 	Memory[0:255]; // 256 32-bit memory
+	output reg [31:0] Read_data; // Contents of memory location at Address
+	reg 	[31:0] 	Memory[0:255]; // 256 32-bit memory
       
    		always 	@(Address or WriteData) // When a signal is received from control, the process starts.
    		begin
    		
 			if (MemWrite==1) begin
-                		Memory[Address>>2] = WriteData[31:0];
+				Memory[Address>>2] = Write_Data[31:0];
 			end
    		end
    		
@@ -26,7 +25,7 @@ module Data_Memory(Address, WriteData, MemWrite, MemRead, Read_data);
 			
 			$display("______Data_memory.v______");
 			$display("Address: %d", Address);
-			$display("Write_data: %d", WriteData);
+			$display("Write_data: %d", Write_Data);
 			$display("Read_data: %d", Read_data);
 			
 			//$display("%h",Memory[Address]);
