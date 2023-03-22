a408e870f4cf9277a31c10f80c6c157055cfd98d
ajinwon1212 <76693616+ajinwon1212@users.noreply.github.com>
2023-03-22 13:22:10 +0900

    Update Data_memory.v

--- MIPS/SINGLE/Data_memory.v
+++ MIPS/SINGLE/Data_memory.v
@@ -5,7 +5,7 @@ module Data_Memory(Address, WriteData, MemWrite, MemRead, Read_data);
     input MemWrite; 		// Control signal for memory write 
     input MemRead; 			// Control signal for memory read 
 
-    output reg[31:0] Read_data; // Contents of memory location at Address
+    output reg [31:0] Read_data; // Contents of memory location at Address
 
     reg 	[31:0] 	Memory[0:255]; // 256 32-bit memory
       
@@ -13,18 +13,23 @@ module Data_Memory(Address, WriteData, MemWrite, MemRead, Read_data);
    		begin
    		
 			if (MemWrite==1) begin
-                Memory[Address>>2] = WriteData[31:0];
+                		Memory[Address>>2] = WriteData[31:0];
 			end
    		end
    		
    		always @(Address or MemRead)
    		begin	
    			if	(MemRead == 1) begin
-                Read_data <= Memory[Address>>2];
-   			end else 
-   				Read_data <= 32'h00000000;
-					
-					//$display("%h",Memory[Address]);
+                		Read_data <= Memory[Address>>2];
+   			end 
+			else Read_data <= 32'h00000000;
+			
+			$display("______Data_memory.v______");
+			$display("Address: %d", Address);
+			$display("Write_data: %d", WriteData);
+			$display("Read_data: %d", Read_data);
+			
+			//$display("%h",Memory[Address]);
    		end 
    		
 			// initial begin
