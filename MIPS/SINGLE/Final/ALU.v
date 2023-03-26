4a7f6f0fed92a70b2ea30c7495f6f50b2c0b54ea
ajinwon1212 <76693616+ajinwon1212@users.noreply.github.com>
2023-03-26 13:25:59 +0900

    Add files via upload

---
+++ MIPS/SINGLE/Final/ALU.v
@@ -0,0 +1,59 @@
+module ALU( 
+	CLK,
+	ALU_IN_1, 
+	ALU_IN_2,
+	ALU_control,
+	Shampt,
+	ALU_zero,
+	ALU_result,
+	Hi,
+	Lo
+);
+	input CLK;
+	input signed [31:0] ALU_IN_1; // 32bit input
+	input signed [31:0] ALU_IN_2; // 32bit input
+	input [3:0] ALU_control; //4bit input
+	input [4:0] Shampt; //5bit input, shift size
+	
+	output ALU_zero; 
+	output signed [31:0] ALU_result;
+
+	output reg signed [31:0] Hi, Lo;
+
+	reg signed [31:0] ALU_Result;
+
+	assign ALU_zero = (ALU_Result == 0) ? ((ALU_control[0] == 1) ? 0 : 1 ) : ((ALU_control[0] == 1) ? 1 : 0 ); 
+	//assign ALU_result = (ALU_control==4'b0111) ? Lo : ((ALU_control==4'b1110)? Hi : ALU_Result);
+	assign ALU_result = ALU_Result;
+
+	always @(*) 
+	begin		
+		ALU_Result <= 32'd0;
+		if (ALU_control==4'b0000) ALU_Result <= ALU_IN_1 + ALU_IN_2; // ADD
+		else if (ALU_control==4'b1000) ALU_Result <= ALU_IN_1 & ALU_IN_2; // AND
+		else if (ALU_control==4'b0100) ALU_Result <= ALU_IN_1 - ALU_IN_2; // SUB (0:sub, beq; 1:bne)
+		else if (ALU_control==4'b0110) ALU_Result <= ALU_IN_1 - ALU_IN_2; // SUB (0:sub, beq; 1:bne)
+		else if (ALU_control==4'b1100) ALU_Result <= ~(ALU_IN_1 | ALU_IN_2); // NOR
+		else if (ALU_control==4'b1010) ALU_Result <= ALU_IN_1 | ALU_IN_2; // OR (ORI)
+		else if (ALU_control==4'b0101) ALU_Result <= ALU_IN_1 < ALU_IN_2 ? 1 : 0; // SLT (SLTI)
+		else if (ALU_control==4'b0010) ALU_Result <= ALU_IN_1 << Shampt; // SHIFT_LEFT (sll)
+		else if (ALU_control==4'b0011) ALU_Result <= ALU_IN_1 >> Shampt; // SHIFT_RIGHT (srl)
+		else if (ALU_control==4'b1011) 
+				begin
+					Lo <= ALU_IN_1 / ALU_IN_2; // division quotient
+					Hi <= ALU_IN_1 % ALU_IN_2;
+					//$display("Hi: %d, Lo: %d", Hi, Lo);
+				end
+		else if (ALU_control==4'b1101)// MULT
+				begin 
+					Lo <= ALU_IN_1 * ALU_IN_2; // MULT
+					Hi <= (ALU_IN_1 * ALU_IN_2) >> 32;
+					//$display("Hi: %d, Lo: %d", Hi, Lo);
+				end
+		else if (ALU_control==4'b1001) ALU_Result <= ALU_IN_1 * ALU_IN_2; //MUL	
+		//else if (ALU_control==4'b0111) ALU_result <= Lo; //mflo
+		//else if (ALU_control==4'b1110) ALU_result <= Hi; //mfhi
+		else if (ALU_control==4'b1111) ALU_Result <= 31'b0; // Do nothing
+	end
+
+endmodule
---
+++ MIPS/SINGLE/Final/ALU_control.v
@@ -0,0 +1,126 @@
+module ALU_control( 
+	ALU_control_IN, // function field for R-type instruction
+	ALUOp, // Aluop
+	ALU_control // 4 bit
+);
+
+	input [5:0] ALU_control_IN; // 6bit input
+	input [2:0] ALUOp; // 3bit input
+		
+	output reg [3:0] ALU_control; // 4 bit output
+
+	always@(ALUOp or ALU_control_IN)
+	begin // I referred to page 261 of the textbook.
+		
+		//$display("______ALU_control.v______");
+		
+		casex({ALUOp,ALU_control_IN})
+		9'b111_xxxxxx: 
+				begin 
+				ALU_control <= 4'b1111;	//Do nothing in ALU
+				$display("[j, jal] ALUop: %b", ALUOp); 
+				end
+		9'b000_xxxxxx: 
+				begin
+				ALU_control <= 4'b0000;	//ADD operation (addi, lw, sw)
+				$display("[addi, lw, sw] ALUop: %b", ALUOp);
+				end
+		9'b100_xxxxxx: 
+				begin
+				ALU_control <= 4'b0100;	//SUB operation 1 (subi, beq)
+				$display("[subi, beq] ALUop: %b", ALUOp);
+				end
+		9'b100_xxxxxx: 
+				begin 
+				ALU_control <= 4'b0110;	//SUB operation 2 (bne)
+				$display("[bne] ALUop: %b", ALUOp);
+				end
+		9'b100_xxxxxx: 
+				begin 
+				ALU_control <= 4'b0101;	//SUB operation 3 (slti)
+				$display("[slti] ALUop: %b", ALUOp);
+				end
+		9'b010_xxxxxx: 
+				begin
+				ALU_control <= 4'b1000;	//AND operation
+				$display("[andi] ALUop: %b", ALUOp);
+				end
+		9'b011_xxxxxx: 
+				begin 
+				ALU_control <= 4'b1010;	//OR operation
+				$display("[ori] ALUop: %b", ALUOp);
+				end
+		/* R type */
+		9'b001_100000: 
+				begin
+				ALU_control <= 4'b0000;	//ADD operation
+				$display("[add] ALUop: %b, funct: %b", ALUOp, ALU_control_IN);
+				end
+		9'b001_100100: 
+				begin
+				ALU_control <= 4'b1000;	//AND operation
+				$display("[and] ALUop: %b, funct: %b", ALUOp, ALU_control_IN);
+				end
+		9'b001_001000: 
+				begin
+				ALU_control <= 4'b1111;	//Do nothing in ALU
+				$display("[jr] ALUop: %b, funct: %b", ALUOp, ALU_control_IN);
+				end
+		9'b001_100111: 
+				begin
+				ALU_control <= 4'b1100;	//NOR operation
+				$display("[nor] ALUop: %b, funct: %b", ALUOp, ALU_control_IN);
+				end
+		9'b001_100101: 
+				begin
+				ALU_control <= 4'b1010;	//OR operation
+				$display("[or] ALUop: %b, funct: %b", ALUOp, ALU_control_IN);
+				end
+		9'b001_101010: 
+				begin
+				ALU_control <= 4'b0101;	//SUB operation 3 (slt)
+				$display("[slt] ALUop: %b, funct: %b", ALUOp, ALU_control_IN);
+				end
+		9'b001_000000: 
+				begin 
+				ALU_control <= 4'b0010;	//SHIFT Left operation (sll)
+				$display("[sll] ALUop: %b, funct: %b", ALUOp, ALU_control_IN);
+				end
+		9'b001_000010: 
+				begin 
+				ALU_control <= 4'b0011;	//SHIFT Right operation (srl)
+				$display("[srl] ALUop: %b, funct: %b", ALUOp, ALU_control_IN);
+				end
+		9'b001_100010: 
+				begin 
+				ALU_control <= 4'b0100;	//SUB operation 1 (sub)
+				$display("[sub] ALUop: %b, funct: %b", ALUOp, ALU_control_IN);
+				end
+		9'b001_011010: 
+				begin
+				ALU_control <= 4'b1011;	//DIVIDE operation (div)
+				$display("[div] ALUop: %b, funct: %b", ALUOp, ALU_control_IN);
+				end
+		9'b001_011000: 
+				begin 
+				ALU_control <= 4'b1101;	//MULTIPLY operation 1 (mult)
+				$display("[mult] ALUop: %b, funct: %b", ALUOp, ALU_control_IN);
+				end
+		9'b001_010000:
+				begin
+				ALU_control <= 4'b1110; //Move From Hi (mfhi)
+				$display("[mfhi] ALUop: %b, funct: %b", ALUOp, ALU_control_IN);
+				end
+		9'b001_010010:
+				begin
+				ALU_control <= 4'b0111; //Move From Lo (mflo)
+				$display("[mflo] ALUop: %b, funct: %b", ALUOp, ALU_control_IN);
+				end
+		9'b001_011001: //Substitude multu > mul in this project!
+				begin
+				ALU_control <= 4'b1001; //MULTIPLY operation 2 (mul)
+				$display("[mul] ALUop: %b, funct: %b", ALUOp, ALU_control_IN);
+				end
+		endcase
+	end
+endmodule
