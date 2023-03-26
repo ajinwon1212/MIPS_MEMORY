295eddc181e693052d4347f1bc4912dc77a5af04
ajinwon1212 <76693616+ajinwon1212@users.noreply.github.com>
2023-03-26 13:24:23 +0900

    Delete MIPS/a directory

--- MIPS/a/Sign_extend.v
+++
@@ -1,7 +0,0 @@
-module Sign_extend(Sign_extend_in, Sign_extend);
-	input [15:0] Sign_extend_in;
-	output [31:0] Sign_extend;
-
-	assign Sign_extend = (Sign_extend_in[15]) ? {16'hffff, Sign_extend_in} : {16'h0000,Sign_extend_in} ;
-
-endmodule
