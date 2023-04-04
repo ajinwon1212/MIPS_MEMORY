dd4c3197962130f7298b36cc9da93b580a265a05
YunSung12 <107446074+YunSung12@users.noreply.github.com>
2023-04-04 22:39:43 +0900

    Update WBMEM_Reg.v

--- MIPS/PIPELINE/WBMEM_Reg.v
+++ MIPS/PIPELINE/WBMEM_Reg.v
@@ -1,13 +1,13 @@
 module WBMEM_Reg (
     input CLK,
     input RESET,
-    input [2:0] WB_MEM,
+    input [4:0] WB_MEM,
     input [5:0] MEM_Opcode,
     input signed [31:0] MEM_ALU_RESULT,
     input signed [31:0] MEM_RD_DATA,
     input [4:0] MEM_RD,
     input [31:0] MEM_PC_4,
-    output reg [2:0] WB,
+    output reg [1:0] WB,
     output reg [5:0] WB_Opcode,
     output reg signed [31:0] WB_ALU_RESULT,
     output reg signed [31:0] WB_RD_Data,
@@ -18,7 +18,7 @@ module WBMEM_Reg (
 always @(posedge CLK or posedge RESET) begin
     if (RESET) begin
         // Reset all registers
-        WB <= 3'b0;
+        WB <= 2'b0;
         WB_Opcode <= 6'b0;
         WB_ALU_RESULT <= 32'sd0;
         WB_RD_Data <= 32'sd0;
@@ -26,7 +26,7 @@ always @(posedge CLK or posedge RESET) begin
         WB_PC_4 <= 32'd0;
     end else begin
         // Transfer values from input to output registers
-        WB <= WB_MEM;
+        WB <= WB_MEM[4:3];
         WB_Opcode <= MEM_Opcode;
         WB_ALU_RESULT <= MEM_ALU_RESULT;
         WB_RD_Data <= MEM_RD_DATA;
