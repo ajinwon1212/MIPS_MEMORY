efe7d74c60526654a94879e3306d49462343db04
mumu-lee <82321285+mumu-lee@users.noreply.github.com>
2023-04-23 12:58:12 +0900

    Update Forwarding_Unit_EX.v

--- MIPS/PIPELINE/AHJIN/Forwarding_Unit_EX.v
+++ MIPS/PIPELINE/AHJIN/Forwarding_Unit_EX.v
@@ -7,12 +7,17 @@ module Forwarding_Unit_EX (
     output [1:0] FW_sig2
 );
 
+wire MEM_Read_EX = (opcode_EX == 6'b100011); // LW instruction
+wire MEM_RegWrite_EX = (opcode_EX != 6'b000100) && (opcode_EX != 6'b101011) && (opcode_EX != 6'b000000 || EX_RT != 5'b00000);
+wire WB_RegWrite = (opcode_EX != 6'b000100) && (opcode_EX != 6'b101011) && (opcode_EX != 6'b000000 || EX_RT != 5'b00000);
 
-assign FW_sig1 = ((EX_RS == MEM_RD) && (EX_RS != 5'b0) && (opcode_EX != 6'b000000) && MEM_FW) ? 2'b01 : 
-                ((EX_RS == WB_RD) && (EX_RS != 5'b0) && (opcode_EX != 6'b000000) && WB_FW) ? 2'b10 : 2'b00;
+assign FW_sig1 = ((EX_RS == MEM_RD) && (EX_RS != 5'b0) && !MEM_Read_EX && MEM_RegWrite_EX && MEM_FW) ? 2'b10 : 
+                ((EX_RS == WB_RD) && (EX_RS != 5'b0) && WB_RegWrite && WB_FW && !((EX_RS == MEM_RD) && MEM_RegWrite_EX && MEM_FW)) ? 2'b01 : 2'b00;
 
-assign FW_sig2 = (opcode_EX == 6'b000100) || (opcode_EX == 6'b000101) && (MEM_RD == EX_RT) ? ((MEM_FW) ? 2'b01 : 2'b00) : 
-                (opcode_EX == 6'b000100) || (opcode_EX == 6'b000101) && (WB_RD == EX_RT) ? ((WB_FW) ? 2'b10 : 2'b00) : 2'b00;
+assign FW_sig2 = ((EX_RT == MEM_RD) && (EX_RT != 5'b0) && !MEM_Read_EX && MEM_RegWrite_EX && MEM_FW) ? 2'b10 : 
+                ((EX_RT == WB_RD) && (EX_RT != 5'b0) && WB_RegWrite && WB_FW && !((EX_RT == MEM_RD) && MEM_RegWrite_EX && MEM_FW)) ? 2'b01 : 2'b00;
 
 endmodule
 
+
+// https://gofo-coding.tistory.com/entry/Data-Hazard
