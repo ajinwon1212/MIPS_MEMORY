7f8e1b77f84ea198da21659fb9039218d9a6a21f
ajinwon1212 <76693616+ajinwon1212@users.noreply.github.com>
2023-04-30 16:24:13 +0900

    Delete soykim directory

--- soykim/single/writeback_module.v
+++
@@ -1,8 +0,0 @@
-
-module Writeback_module(Readata, ALUresult, MemToReg, outdata);
-	input [31:0] Readata, ALUresult;
-	input MemToReg;
-	output [31:0] outdata;
-	
-	assign outdata = (MemToReg == 1'b1) ? Readata : ALUresult;
-endmodule 
