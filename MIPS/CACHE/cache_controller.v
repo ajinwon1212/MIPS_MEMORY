477a5a667bf972b98baac4192c8f707ab7ad7e26
ajinwon1212 <76693616+ajinwon1212@users.noreply.github.com>
2023-05-23 12:34:40 +0900

    Update cache_controller.v

--- MIPS/CACHE/cache_controller.v
+++ MIPS/CACHE/cache_controller.v
@@ -4,7 +4,7 @@ module cache_controller(CLK, HitWrite, Access_MM);
     
     output reg Access_MM;
 
-    always@(posedge clk)
+    always@(posedge CLK)
     begin
         if(HitWrite == 1) begin
             Access_MM = 0;
