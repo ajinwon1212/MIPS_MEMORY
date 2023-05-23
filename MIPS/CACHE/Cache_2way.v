0cc87989a176f8e50d7b11ab743431553abbb8d2
ajinwon1212 <76693616+ajinwon1212@users.noreply.github.com>
2023-05-23 12:41:51 +0900

    Update Cache_2way.v

--- MIPS/CACHE/Cache_2way.v
+++ MIPS/CACHE/Cache_2way.v
@@ -28,7 +28,41 @@ module Cache_Fully(CLK, RESET, PC, Access_MM, Data_MM, HitWrite, Data_Cache, CNT
                 set <= PC[3:2];
                 if (!RESET) begin
                         set <= 2'd0;
+                        cache1[0] <= 61'd0;
+                        cache1[1] <= 61'd0;
+                        cache1[2] <= 61'd0;
+                        cache1[3] <= 61'd0;
+                        cache2[0] <= 61'd0;
+                        cache2[1] <= 61'd0;
+                        cache2[2] <= 61'd0;
+                        cache2[3] <= 61'd0;
                 end
+                else begin
+                        if(Access_MM) begin
+                                ;
+                        end
+                        else if(!Access_MM) begin
+                                if(PC[31:5] == cache1[set][59:33]) begin
+                                        if(cache1[set][32] == 1'b1) begin
+                                           HitWrite <= 1'b1;
+                                                Data_Cache <= cache1[set][31:0];
+                                                CNT_HIT <= CNT_HIT + 1;
+                                        end
+                                        else begin
+                                                HitWrite <= 1'b0;
+                                                CNT_MISS <= CNT_MISS + 1;
+                                                Data_Cache <= 32'd0;
+                                        end
+                                end
+                                else if(PC[31:5] == cache2[set][59:33]) begin
+                                        
+                                end
+                                else begin
+                                        
+                                end
+                        end
+                end
+
         end
   
   
