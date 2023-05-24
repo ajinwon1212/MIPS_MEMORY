0295c8cd27f48add6754107d7844cfc3930d8d69
YunSung12 <107446074+YunSung12@users.noreply.github.com>
2023-05-24 11:11:08 +0900

    Update Cache_Direct_Multiword.v

--- MIPS/CACHE/Cache_Direct_Multiword.v
+++ MIPS/CACHE/Cache_Direct_Multiword.v
@@ -15,27 +15,22 @@ module Cache_Direct_Multiword(CLK, RESET, PC, index, Access_MM, Data_MM, HitWrit
 	input RESET;
 	input [31:0] PC;
 	input [1:0] index;
-	input block_offset;
+	input block_offset
 	input Access_MM; //0: Read Data from MM sig
 	input [63:0] Data_MM; //Read Data from MM
 
 	output reg HitWrite; //Hit, PCWrite, IFIDWrite
 	output reg [31:0] Data_Cache;
 	output reg [19:0] CNT_HIT, CNT_MISS; //Counter for Checking
-	//output [60:0] CACHE;
+
 	output reg [1:0] CONT;
 
-	//output reg [2:0] index;
 	reg [91:0] cache [3:0]; 
-	//Data: [31:0], Valid: [32], Tag: [59:33]
- 	//wire CACHE;
+
 
 	always@(posedge CLK, posedge RESET)
 	begin
-		//index <= PC[4:3];
-		//block_offset <= PC[2];
 		if (RESET) begin
-			//index <= 3'd0;
 			cache[0] <= 92'd0;
 			cache[1] <= 92'd0;
 			cache[2] <= 92'd0;
@@ -46,34 +41,38 @@ module Cache_Direct_Multiword(CLK, RESET, PC, index, Access_MM, Data_MM, HitWrit
         	end
         	else begin
 			if(Access_MM) begin
-				cache[index][32] <= 1'b1; //Valid =1
-				cache[index][31:0] <= Data_MM;
-				cache[index][59:33] <= PC[31:5]; //Tag
+				cache[index][64] <= 1'b1; //Valid =1
+				cache[index][63:0] <= Data_MM;
+				cache[index][91:65] <= PC[31:5]; //Tag
 				Data_Cache <= Data_MM;
 				HitWrite <= 1'b1;
 				CONT <= 2'd0;
 			end
 			else if (!Access_MM) begin
-				if(PC[31:5] == cache[index][59:33]) begin
-					if (cache[index][32] == 1'b1) begin
+				if(cache[index][64] == 1'b0) begin
+					HitWrite <= 1'b0;
+					CNT_MISS <= CNT_MISS +1;
+					Data_Cache <= 32'd0;  
+					CONT <= 2'd2; 
+				end
+				else begin
+					if (PC[31:5] == cache[index][91:65]) begin
 						HitWrite <= 1'b1;
-						Data_Cache <= cache[index][31:0];
 						CNT_HIT <= CNT_HIT+1;
 						CONT <= 2'd1;
+						if (block_offset == 1'b0) 
+							Data_Cache <= cache[index][63:32];
+						else
+							Data_Cache <= cache[index][31:0];
 					end
 					else begin
 						HitWrite <= 1'b0;
 						CNT_MISS <= CNT_MISS +1;
 						Data_Cache <= 32'd0;  
-						CONT <= 2'd2;                      
+						CONT <= 2'd3;            
 					end
 				end
-                		else begin 
-					HitWrite <= 1'b0;
-					CNT_MISS <= CNT_MISS +1;
-					Data_Cache <= 32'd0;
-					CONT <= 2'd3;
-               			end
+                		
 			end
 		end
 	end
